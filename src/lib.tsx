3efce69fe78b908da8f9ee71705981d98d8bab1b
Graham Abbott <graham.abbott@gmail.com>
2016-11-24 13:03:41 -0800

    debounce in ts and a map merge for the std lib.

--- src/lib.tsx
+++ src/lib.tsx
@@ -1,3 +1,41 @@
+// Taken directly from `npm install debounce` because it doesn't have clean types.
+function debounce(func: Function, wait: number, immediate?: boolean): (Function) => void {
+    function now(): number {
+        return (new Date()).getTime();
+    };
+
+    var timeout, args, context, timestamp, result;
+    if (null == wait) wait = 100;
+
+    function later() {
+        var last = now() - timestamp;
+
+        if (last < wait && last > 0) {
+            timeout = setTimeout(later, wait - last);
+        } else {
+            timeout = null;
+            if (!immediate) {
+                result = func.apply(context, args);
+                if (!timeout) context = args = null;
+            }
+        }
+    };
+
+    return function debounced() {
+        context = this;
+        args = arguments;
+        timestamp = now();
+        var callNow = immediate && !timeout;
+        if (!timeout) timeout = setTimeout(later, wait);
+        if (callNow) {
+            result = func.apply(context, args);
+            context = args = null;
+        }
+
+        return result;
+    };
+};
+
 function ensureValidKeys(state: any, ensureKeys: Array<string>): any {
     var LOADED_KEY = "is_loaded";
     var return_state: any = {};
@@ -16,7 +54,19 @@ function ensureValidKeys(state: any, ensureKeys: Array<string>): any {
     return_state["is_loaded"] = true;
 
     return return_state;
-}
+};
+
+function mapMerge(left: any, right: any): any {
+    var new_map = {};
+
+    for (let source of [left, right]) {
+        for (let key of source) {
+            new_map[key] = source[key];
+        }
+    }
+
+    return new_map
+};
 
 function test_lib_working() {
     console.log("Lib is working!");
@@ -24,5 +74,7 @@ function test_lib_working() {
 
 export {
     test_lib_working,
-    ensureValidKeys
+    ensureValidKeys,
+    debounce,
+    mapMerge
 }
