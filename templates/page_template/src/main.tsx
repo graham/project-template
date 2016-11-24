5e626ad9bdd83ada21b4c579a87d0cfdf8587c88
Graham Abbott <graham.abbott@gmail.com>
2016-11-24 08:21:32 -0800

    adding a template

---
+++ templates/page_template/src/main.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { test_lib_working } from "myproj-lib";
+
+class HelloWorld extends React.Component<{}, {}> {
+    render() {
+        return <div> Hello World; from home! </div>;
+    }
+}
+
+ReactDOM.render(<HelloWorld />,
+    document.getElementById('content'));
+
+test_lib_working();
