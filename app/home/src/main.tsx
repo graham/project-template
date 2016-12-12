7d24ec181929de5163d0cb767a24a36797a9716d
Graham Abbott <graham.abbott@gmail.com>
2016-12-11 16:21:24 -0800

    continued attempts to make inferno work have failed.

--- app/home/src/main.tsx
+++ app/home/src/main.tsx
@@ -1,14 +1,3 @@
 import Inferno from 'inferno';
-import Component from 'inferno-component';
 
-import { test_lib_working } from "myproj-lib";
-
-class HelloWorld extends Component<{}, {}> {
-    render() {
-        return <div> Hello World; from home! </div>;
-    }
-}
-
-Inferno.render(<HelloWorld />, document.getElementById('content'));
-
-test_lib_working();
+Inferno.render(<div>inside the div</div>, document.body);
--- templates/page_template/src/main.tsx
+++ templates/page_template/src/main.tsx
@@ -3,7 +3,8 @@ import Component from 'inferno-component';
 
 import { test_lib_working } from "myproj-lib";
 
-class HelloWorld extends Component<{}, {}> {
+class HelloWorld extends Component {
+    
     render() {
         return <div> Hello World; from home! </div>;
     }
